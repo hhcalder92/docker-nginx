4ad67056976968c84d9dd38a11baa4e60be1703b
root <root@ip-172-31-38-253.us-west-2.compute.internal>
2016-12-17 03:58:55 +0000

    nginx config

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,22 @@
 FROM nginx:latest
 
+RUN apt-get update && apt-get install -y \
+	curl \
+	sudo
+
 COPY nginx.conf /etc/nginx/nginx.conf
 
-COPY  nginx_site_available /etc/nginx/site_available/default
+COPY  nginx_site_available /etc/nginx/sites-available/default
+
+COPY scripts /scripts 
+
+RUN /scripts/configure.sh
 
 # Deps 
 RUN apt-get update && apt-get install -y \
         git \
+	python-software-properties \
         nodejs \
         npm 
-
-COPY scripts /scripts 
-
-RUN /scripts/
 
 CMD ["nginx"]
--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-DOMAIN=staging.artmart.city
-PROJECT=artmart-city-frontend
-BACKEND_APP_NAME=amc-api
-WEB_APP_NAME=amc-web-app
-APP_DIR=/web
-BACKEND_DIR=$APP_DIR/$BACKEND_APP_NAME
-WEB_APP_DIR=$APP_DIR/$WEB_APP_NAME
-MEDIA_DIR=$APP_DIR/media/
-STATIC_DIR=$APP_DIR/static/
+source /scripts/env
 
+mkdir -p /etc/nginx/{sites-available,sites-enabled}
 
-ln -s /etc/nginx/sites-available/$PROJECT		/etc/nginx/sites-enabled/$PROJECT
+ln -s /etc/nginx/sites-available/default		/etc/nginx/sites-available/$PROJECT
+ln -s /etc/nginx/sites-available/default		/etc/nginx/sites-enabled/$PROJECT
 
-sed -i -e "s/{{domain}}/$DOMAIN/g"			/etc/nginx/sites-available/$PROJECT
-sed -i -e "s/{{backend_directory}}/$BACKEND_DIR/g"	/etc/nginx/sites-available/$PROJECT
-sed -i -e "s/{{static_directory}}/$STATIC_DIR/g"	/etc/nginx/sites-available/$PROJECT
-sed -i -e "s/{{media_directory}}/$MEDIA_DIR/g"		/etc/nginx/sites-available/$PROJECT
-sed -i -e "s/{{web_app_directory}}/$WEB_APP_DIR/g"	/etc/nginx/sites-available/$PROJECT
+sed -i -e "s/{{domain}}/$DOMAIN/g"			/etc/nginx/sites-available/default
+sed -i -e "s#{{backend_directory}}#$BACKEND_DIR#g"	/etc/nginx/sites-available/default
+sed -i -e "s#{{static_directory}}#$STATIC_DIR#g"	/etc/nginx/sites-available/default
+sed -i -e "s#{{media_directory}}#$MEDIA_DIR#g"		/etc/nginx/sites-available/default
+sed -i -e "s#{{web_app_directory}}#$WEB_APP_DIR#g"	/etc/nginx/sites-available/default
 
-mkdir -p $APP_DIR
-cd $APP_DIR
-
-[[ ! -d $PROJECT ]] && git clone http://docker@calderon.solutions/git/r/art/${PROJECT}.git $PROJECT
-
-cd $PROJECT
-
-npm install
